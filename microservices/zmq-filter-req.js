1c3341edae2eff78463e1db79bc1e93cecfc7200
ajafik <oluwafikayoajayi@gmail.com>
2018-06-28 17:34:49 +0100

    Added REQ/RES messaging pattern

---
+++ microservices/zmq-filter-req.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const zmq = require('zeromq');
+const filename  = process.argv[2];
+
+
+//Create Requester endpoint 
+const requester  = zmq.socket('req');
+
+//Handles reploes from the responder.
+requester.on('message', data => {
+    const response  = JSON.parse(data);
+    console.log(`Received response `, response);
+});
+
+requester.connect('tcp://localhost:60401');
+
+//Send a request for content
+console.log(`Sending a request for ${filename}`);
+
+requester.send(JSON.stringify({path: filename}));
