974a361da8fe659470870542efb14fa28e2fbbca
ajafik <oluwafikayoajayi@gmail.com>
2018-07-03 11:18:01 +0100

    Added more functionalities to the ESCLU

--- esclu/index.js
+++ esclu/index.js
@@ -6,6 +6,7 @@ const pkg = require('./package.json');
 
 const fullUrl = (path = '') => {
     let url = `http://${program.host}:${program.port}/`;
+
     if (program.index) {
         url += program.index + '/';
         if (program.type) {
@@ -27,6 +28,47 @@ program.version(pkg.version).description(pkg.description).usage('[options] <comm
 program.command('url [path]')
     .description('generate the URL for the options and path (default is /)')
     .action((path = '/') => console.log(fullUrl(path)));
+
+program.command('get [path]').description('perform an HTTP GET request for path  (default is /)').action((path = '/') => {
+    const options = {
+        url: fullUrl(path),
+        json: program.json,
+    };
+    request(options, (err, res, body) => {
+        if (program.json) {
+            console.log(JSON.stringify(err || body));
+        } else {
+            if (err) throw err;
+            console.log(body);
+        }
+    })
+});
+
+
+const handleResponse = (err, res, body) => {
+    if (program.json) {
+        console.log(JSON.stringify(err || body));
+    } else {
+        if (err) throw err;
+        console.log(body);
+    }
+};
+
+program.command('create-index').description('create an index').action(() => {
+    if (!program.index) {
+        const msg = 'No index specified! Use --index <name>';
+        if (!program.json) throw Error(msg);
+        console.log(JSON.stringify({ error: msg }));
+        return;
+    }
+    request.put(fullUrl(), handleResponse);
+});
+
+program.command('list-indices').alias('li').description('get a list of indices in this cluster')
+    .action(() => {
+        const path = program.json ? '_all' : '_cat/indices?v';
+        request({ url: fullUrl(path), json: program.json }, handleResponse);
+    });
 
 program.parse(process.argv);
 
